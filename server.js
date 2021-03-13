3504f84a485f49e70d77cfb71bd907e754a01023
jitochadha <jitochadha@gmail.com>
2021-03-13 00:24:34 -0800

    post get delete methods working via routes

--- server.js
+++ server.js
@@ -17,37 +17,72 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('connected to the election database.');
 });
 
-// db.all(`SELECT * FROM candidates`, (err, rows) => {
-// console.log(rows);
-// });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(`SELECT * FROM candidates`, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
-//Get a single candidate
-db.get(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(row);
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates
+                WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
 });
 
 // Delete a candidate
-// db.run(`DELETE FROM candidates WHERE id=?`, 1, function (err, result) {
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+});
+
+// ES5 function, not arrow function, to use this
+// db.run(sql, params, function (err, result) {
 //     if (err) {
 //         console.log(err);
 //     }
-//     console.log(result, this, this.changes);
+//     console.log(result, this.lastID);
 // });
-
-// Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-                VALUES(?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function (err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result, this.lastID);
-});
 
 
 //Default response for any other requests(Not found) Catch all
